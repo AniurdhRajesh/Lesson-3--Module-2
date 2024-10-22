d8872aa55abed78735784b61b32ab954698f3e05
AniurdhRajesh <anirudhrajesh04@gmail.com>
2024-10-22 18:45:41 +0530

    Lesson 3-module 2

---
+++ Infinity.py
@@ -0,0 +1,9 @@
+while True:
+ x=int(input("Enter the Terms : "))
+ i=0
+ sum=0
+ while i<=x:
+   i+=1
+   sum+=i
+   print(i)
+ print("Sum of all the umbers are : ",sum)
---
+++ Sum of natural no.s.py
@@ -0,0 +1,8 @@
+x=int(input("Enter the Terms : "))
+i=0
+sum=0
+while i<=x:
+    i+=1
+    sum+=i
+    print(i)
+print("Sum of all the umbers are : ",sum)
